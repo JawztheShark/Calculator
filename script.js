902985a0c29c80384e4f3f0784b3d58cdf4f8c7d
JawztheShark <87725414+JawztheShark@users.noreply.github.com>
2021-08-11 17:37:27 +0000

    changed names

--- script.js
+++ script.js
@@ -1,66 +1,89 @@
+//base display
 let output = 0;
+//array
 let arr =[];
+//one
 document.getElementById('one').onclick = function (e) {
 arr.push(1);
 document.getElementById("display").innerHTML = arr.join('');
 }
+//two
 document.getElementById('two').onclick = function (e) {
 arr.push(2);
 document.getElementById("display").innerHTML = arr.join('');
 }
+//three
 document.getElementById('three').onclick = function (e) {
 arr.push(3);
 document.getElementById("display").innerHTML = arr.join('');
 }
+//four
 document.getElementById('four').onclick = function (e) {
 arr.push(4);
 document.getElementById("display").innerHTML = arr.join('');
 }
+//five
 document.getElementById('five').onclick = function (e) {
 arr.push(5);
 document.getElementById("display").innerHTML = arr.join('');
 }
+//six
 document.getElementById('six').onclick = function (e) {
 arr.push(6);
 document.getElementById("display").innerHTML = arr.join('');
 }
+//seven
 document.getElementById('seven').onclick = function (e) {
 arr.push(7);
 document.getElementById("display").innerHTML = arr.join('');
 }
+//eight
 document.getElementById('eight').onclick = function (e) {
 arr.push(8);
 document.getElementById("display").innerHTML = arr.join('');
 }
+//nine
 document.getElementById('nine').onclick = function (e) {
 arr.push(9);
 document.getElementById("display").innerHTML = arr.join('');
 }
+//divide
 document.getElementById('divide').onclick = function (e) {
-arr.push('/');
+arr.push("div");
 document.getElementById("display").innerHTML = arr.join('');
 }
+//times
 document.getElementById('times').onclick = function (e) {
-arr.push('*');
+arr.push("mult");
 document.getElementById("display").innerHTML = arr.join('');
 }
+//minus
 document.getElementById('minus').onclick = function (e) {
-arr.push('-');
+arr.push("min");
 document.getElementById("display").innerHTML = arr.join('');
 }
+//plus
 document.getElementById('plus').onclick = function (e) {
-arr.push('+');
+arr.push("plus");
 document.getElementById("display").innerHTML = arr.join('');
 }
+//decimal
 document.getElementById('decimal').onclick = function (e) {
-arr.push('.');
+arr.push("dec");
 document.getElementById("display").innerHTML = arr.join('');
 }
+//equals
 document.getElementById('equals').onclick = function (e) {
+console.log(arr.join(""))
+let a = arr.join("")
+console.log(a)
 document.getElementById("display").innerHTML = arr.join('');
 }
+//clear
 document.getElementById('clear').onclick = function (e) {
-arr = [0];
-document.getElementById("display").innerHTML = arr.join('');
-}
+arr = [];
 document.getElementById("display").innerHTML = output;
+}
+//base display
+document.getElementById("display").innerHTML = output;
+
